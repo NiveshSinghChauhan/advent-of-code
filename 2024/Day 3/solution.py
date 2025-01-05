cccf6bc903b59738183437bea297ce941d7f2ead
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2025-01-05 13:25:34 +0530

    Solution 2024 day 3 part 2

--- 2024/Day 3/solution.py
+++ 2024/Day 3/solution.py
@@ -8,10 +8,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(f"{dir_path}/input.txt") as file:
     input_string = file.read()
 
-matches = re.finditer("(?<=mul\()\d+,\d+(?=\))", input_string, re.MULTILINE)
+matches = re.finditer(
+    "(?<=mul\()\d+,\d+(?=\))|do\(\)|don't\(\)", input_string, re.MULTILINE
+)
 
 result = 0
+should_multiply = True
+
 for match in matches:
+
+    instruction = match.group()
+
+    if instruction == "do()":
+        should_multiply = True
+        continue
+    elif instruction == "don't()":
+        should_multiply = False
+        continue
+
+    if not should_multiply:
+        continue
+
     num1, num2 = match.group().split(",")
     result += int(num1) * int(num2)
 
